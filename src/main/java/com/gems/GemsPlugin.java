d73db0fd30d469767b35d32332e3f901c470b4c6
pl140hz-max <pl140hz@gmail.com>
2026-07-30 18:35:11 -0300

    Refactor GemsPlugin class for cleaner implementation
    Removed unused variables and updated log messages.

--- src/main/java/com/gems/GemsPlugin.java
+++ src/main/java/com/gems/GemsPlugin.java
@@ -1,26 +1,17 @@
 package com.gems;
 
-import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GemsPlugin extends JavaPlugin {
 
-    private NamespacedKey gemKey;
-    // Coloque aqui o link do download direto da sua texture pack (arquivo .zip)
-    private static final String RESOURCE_PACK_URL = "https://exemplo.com/gems_resource_pack.zip"; 
-
     @Override
     public void onEnable() {
-        this.gemKey = new NamespacedKey(this, "elemental_gem");
         getServer().getPluginManager().registerEvents(new GemListener(this), this);
-        getLogger().info("GemsPlugin enabled successfully!");
+        getLogger().info("ElementalSMP ativado com sucesso!");
     }
 
-    public NamespacedKey getGemKey() {
-        return gemKey;
-    }
-
-    public String getResourcePackUrl() {
-        return RESOURCE_PACK_URL;
+    @Override
+    public void onDisable() {
+        getLogger().info("ElementalSMP desativado.");
     }
 }
