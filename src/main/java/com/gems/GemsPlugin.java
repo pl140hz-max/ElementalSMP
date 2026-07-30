fda2d28551bb1a396eaff5e853f41d9e35ef6307
pl140hz-max <pl140hz@gmail.com>
2026-07-30 19:00:26 -0300

    Add resource pack URL handling in GemsPlugin
    Added getter and setter for resource pack URL.

--- src/main/java/com/gems/GemsPlugin.java
+++ src/main/java/com/gems/GemsPlugin.java
@@ -4,6 +4,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class GemsPlugin extends JavaPlugin {
 
+    // Direct link to your .zip resource pack (leave empty until it's ready)
+    private String resourcePackUrl = "";
+
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new GemListener(this), this);
@@ -13,5 +16,13 @@ public class GemsPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("ElementalSMP disabled.");
+    }
+
+    public String getResourcePackUrl() {
+        return resourcePackUrl;
+    }
+
+    public void setResourcePackUrl(String resourcePackUrl) {
+        this.resourcePackUrl = resourcePackUrl;
     }
 }
